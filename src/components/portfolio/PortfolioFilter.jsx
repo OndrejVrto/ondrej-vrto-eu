46d01672e947b4ffffd030703edb15c8aeaca985
Ondrej Vrťo <32365016+OndrejVrto@users.noreply.github.com>
2024-01-31 14:28:09 +0100

    update css responsive

--- src/components/portfolio/PortfolioFilter.jsx
+++ src/components/portfolio/PortfolioFilter.jsx
@@ -18,30 +18,35 @@ export default function PortfolioFilter({ handleFilter }) {
     return (
         <div id="portfolio-filter">
             <button
+                aria-label="Filtrovať výsledky podľa kľúčového slova: Konfigurátor"
                 onClick={() => handleTag("configurator")}
                 className={getClassButton("configurator")}
             >
                 Konfigurátory
             </button>
             <button
+                aria-label="Filtrovať výsledky podľa kľúčového slova: Administrácia"
                 onClick={() => handleTag("admin")}
                 className={getClassButton("admin")}
             >
                 Administrácia na mieru
             </button>
             <button
+                aria-label="Filtrovať výsledky podľa kľúčového slova: Balíček"
                 onClick={() => handleTag("package")}
                 className={getClassButton("package")}
             >
                 Balíčky
             </button>
             <button
+                aria-label="Filtrovať výsledky podľa kľúčového slova: Web podľa designu"
                 onClick={() => handleTag("web")}
                 className={getClassButton("web")}
             >
                 Web podľa designu
             </button>
             <button
+                aria-label="Resetovať filter"
                 onClick={() => handleTag(null)}
                 className={tag === null ? "reset active" : "reset"}
             >
